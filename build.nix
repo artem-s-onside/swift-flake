fe60d85cd47e53a17e718d99d799bc11afb22490
Timothy Klim <git@timothyklim.com>
2024-10-06 06:35:55 +0000

    fix: expose swift-symbolgraph-extract and swift-autolink-extract

--- build.nix
+++ build.nix
@@ -103,6 +103,10 @@ stdenv.mkDerivation (wrapperParams // {
   installPhase = ''
     cp -R . $out
     mkdir $out/bin
+
+    for progName in swift-symbolgraph-extract swift-autolink-extract; do
+      ln -s $out/usr/bin/swift-frontend $out/bin/$progName
+    done
   '' + lib.optionalString replaceClang ''
     rm -rf $out/usr/bin/clang-17 $out/usr/bin/clangd $out/usr/lib/clang
 
