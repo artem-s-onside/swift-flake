40fbede7655caac0552f3ec34a56312e0d72742a
timothy <git@timothyklim.com>
2025-02-08 17:24:14 +0700

    chore: add snapshot version

--- flake.nix
+++ flake.nix
@@ -13,10 +13,27 @@
       url = "https://download.swift.org/swift-6.0.3-release/xcode/swift-6.0.3-RELEASE/swift-6.0.3-RELEASE-osx.pkg";
       flake = false;
     };
+
+    swift_snapshot_linux = {
+      url = "https://download.swift.org/swift-6.1-branch/ubi9/swift-6.1-DEVELOPMENT-SNAPSHOT-2025-02-07-a/swift-6.1-DEVELOPMENT-SNAPSHOT-2025-02-07-a-ubi9.tar.gz";
+      flake = false;
+    };
+    swift_snapshot_macos = {
+      url = "https://download.swift.org/swift-6.1-branch/xcode/swift-6.1-DEVELOPMENT-SNAPSHOT-2025-02-07-a/swift-6.1-DEVELOPMENT-SNAPSHOT-2025-02-07-a-osx.pkg";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, swift_60_linux, swift_60_macos }:
-    with flake-utils.lib; with system; eachSystem [ x86_64-linux aarch64-linux aarch64-darwin ] (system:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , swift_60_linux
+    , swift_60_macos
+    , swift_snapshot_linux
+    , swift_snapshot_macos
+    }:
+      with flake-utils.lib; with system; eachSystem [ x86_64-linux aarch64-linux aarch64-darwin ] (system:
       let
         sources = (nixpkgs.lib.importJSON ./flake.lock).nodes;
         pkgs = nixpkgs.legacyPackages.${system};
@@ -27,10 +44,20 @@
             else throw "Unsupproted system: ${stdenv.hostPlatform.system}";
           version = "6.0.3";
         };
+        swift_snapshot = with pkgs; callPackage ./build.nix {
+          src =
+            if stdenv.hostPlatform.system == "x86_64-linux" then swift_snapshot_linux
+            else if stdenv.hostPlatform.system == "aarch64-darwin" then swift_snapshot_macos
+            else throw "Unsupproted system: ${stdenv.hostPlatform.system}";
+          version = "6.1-DEVELOPMENT-SNAPSHOT-2025-02-07";
+        };
         derivation = { inherit swift; };
       in
       rec {
-        packages = derivation // { default = swift; };
+        packages = derivation // {
+          default = swift;
+          swift_snapshot = swift_snapshot;
+        };
         legacyPackages = pkgs.extend overlay;
         nixosModules.default = {
           nixpkgs.overlays = [ overlay ];
